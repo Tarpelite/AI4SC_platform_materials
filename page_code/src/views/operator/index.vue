4131d4a621d53dd84d6925333a257af36b4f4304
tianyuc <tianyuc@buaa.edu.cn>
2024-06-30 19:26:30 +0800

    fix some bugs

--- page_code/src/views/operator/index.vue
+++ page_code/src/views/operator/index.vue
@@ -3,15 +3,15 @@
   <div class="buaa-operator">
     <div class="operator-intro clearfix">
       <div class="intro-left">
-        <div class="intro-title">共性框架</div>
-        <div class="intro-subtitle">异构混合的基础框架与计算库，通过算子适配、计算图构建、计算库加速等技术，为共性平台提供基础能力支持。</div>
+        <div class="intro-title">科学智算共性框架</div>
+        <div class="intro-subtitle">国产化、模块化、可扩展、全流程的模型算子开发基础框架</div>
         <div class="intro-content clearfix">
-          <span :class="collapse ? 'content-span' : ''">通过算子适配、计算图构建、计算库加速等技术，为共性平台提供基础能力支持。科学智算算子异构混合适配技术，聚焦实现复杂算子在异构芯片混合架构的可扩展适配，突破基础算子扩展、高阶自动微分算子拆分、复杂算子适配等难题；高阶计算图自动构建技术，实现复杂科学智算流程的灵活表示和高效执行；研究多领域计算库的设计与任务加速，设计模块化的科学计算库，突破智能方程求解和仿真模拟任务的加速计算。异构混合的基础框架与计算库是一套支持模块化、可扩展、全流程的模型算子开发基础框架和服务于科学智算任务的多领域基础计算库，为人工智能科学计</span>
-          <span class="intro-op" :class="collapse ? 'collapse' : ''" @click="collapse = !collapse">{{ collapse ? '展开' : '折叠' }}<img :src="images.cal_arrow_theme" alt=""></span>
+          <span :class="collapse ? 'content-span' : ''">科学智算异构混合的基础框架与计算库是一套支持国产化硬件、模块化、可扩展、全流程的模型算子开发基础框架和服务于科学智算任务的多领域基础计算库，为人工智能科学计算提供通用的基础框架和丰富的计算库等基础能力支撑。包含智算算子异构混合适配、科学智算基础框架、多领域计算库三个模块。智算算子异构混合适配模块基于基础算子扩展、高阶算子拆分、复杂算子多硬件适配等技术，模块化支持算子扩展，为科学智算任务提供软硬件环境支撑。科学智算基础框架提供高阶计算图自动构建、神经网络编译器等机制，提供复杂科学智算任务的灵活表示和高效执行，解决科学智算任务运算过程中高阶微分计算复杂、计算规模大的问题。多领域计算库提供常用的基础数学、方程求解、专业软件协作等基础运算库，加速领域领域任务的设计与实现，实现对多领域科学计算应用的有力支撑。目前已经在海光CPU+海光DCU上支持6类科学智算算子，并使用高阶计算图和编译器优化技术进行加速，提供10类方程求解与10类计算库，高效支撑了工业设计、智能制造、气象预测、地球科学等领域任务。</span>
+          <!-- <span class="intro-op" :class="collapse ? 'collapse' : ''" @click="collapse = collapse">{{ collapse ? '展开' : '折叠' }}<img :src="images.cal_arrow_theme" alt=""></span> -->
         </div>
       </div>
       <div class="intro-right">
-        <img :src="images.datasetBanner" alt="">
+        <img src="https://cnai4sc.oss-cn-beijing.aliyuncs.com/framework_tiny.png" alt="">
       </div>
     </div>
     <div class="calculate-wrapper clearfix">
@@ -64,15 +64,15 @@ export default {
         {title: 'PINN求解方法计算库',id: 48, desc: '集成PINN求解方法相关的计算库，包括多种常见微分方程求解，包括Allen-Cahn、Euler-Beam、Navier-Stokes等。'},
         {title: 'FNO求解方法计算库', id: 49,desc: '集成FNO求解方法相关的计算库，包括FourcastNet等模型'},
         {title: 'DeepONet计算库',id: 50, desc: '集成算子学习相关方法的计算库，包括DeepONet等模型'},
-        {title: '势函数模拟+LAMMPS计算库',id: 51, desc: '集成势函数模拟相关方法的计算库，包括DeepMD-kit套件'}
+        {title: '势函数模拟/LAMMPS计算库',id: 51, desc: '集成势函数模拟相关方法的计算库，包括DeepMD-kit套件'}
       ],
       comList: [
-        {title: '高阶微分类', id: 2, desc: '描述描述描述描述描述描述描述描述'},
-        {title: '复数操作类', id: 1, desc: '描述描述描述描述描述描述描述描述'},
-        {title: '傅里叶变换类', id: 4, desc: '描述描述描述描述描述描述描述描述'},
-        {title: '线性代数类',id: 8,  desc: '描述描述描述描述描述描述描述描述'},
-        {title: '分数阶微分类', id: 13, desc: '描述描述描述描述描述描述描述描述'},
-        {title: '积分操作类', id: 5, desc: '描述描述描述描述描述描述描述描述'},
+        {title: '高阶微分类', id: 2, desc: '海森矩阵、张量操作等'},
+        {title: '复数操作类', id: 1, desc: '计算相位角、复数取模等'},
+        {title: '傅里叶变换类', id: 4, desc: '二维离散傅里叶变换、逆变换等'},
+        {title: '线性代数类',id: 8,  desc: '特征值与特征向量计算等'},
+        {title: '分数阶微分类', id: 13, desc: '分数阶快速、精准微分等'},
+        {title: '积分操作类', id: 5, desc: '蒙特卡洛积分、高斯求积等'},
       ]
     }
   },
@@ -99,7 +99,6 @@ export default {
 
     .intro-left {
       .intro-title {
-        width: 160px;
         height: 56px;
         font-family: Helvetica Neue, Helvetica Neue;
         font-weight: bold;
