e76badc7f97c882b6d015357aa04ec2a16f0e332
fangzhen <fangzhen@benlai.com>
2024-07-03 16:39:22 +0800

    【登录页】登录页跳转

--- page_code/src/views/login/index.vue
+++ page_code/src/views/login/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="box">
     <div class="left">
-        <div class="bg">
-            <img :src="images.loginLogo" alt="">
-            <div class="bgtext">版权所有：科学智算共性平台 京ICP备18020905号-2</div>
-        </div>
+      <div class="bg">
+        <img @click="toHome" :src="images.loginLogo" alt="">
+        <div class="bgtext">版权所有：科学智算共性平台 京ICP备18020905号-2</div>
+      </div>
     </div>
     <div class="right">
-        <Login />
+      <Login/>
     </div>
   </div>
 </template>
@@ -15,6 +15,7 @@
 <script>
 import images from "@/utils/js/exportImage";
 import Login from "./login.vue"
+
 export default {
   data() {
     return {
@@ -26,43 +27,58 @@ export default {
     Login
   },
   methods: {
-    itemClick(val) {},
+    itemClick(val) {
+    },
+    toHome() {
+      this.$router.replace('/')
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .box {
-    width: 100%;
-    display: flex;
-    height: calc(100vh);
-    .left {
-        width: 640px;
-        background: linear-gradient( 170deg, #33ADFF 0%, #45A8FD 40%, #9D2EFF 100%);
-        .bg {
-            height: calc(100vh);
-            background-image: url("@/utils/image/loginBg.png");
-            background-size:100% 100%;
-            position: relative;
-            img {
-                width: 648px;
-                height: 690px;
-            }
-            .bgtext {
-                position: absolute;
-                left: 0;
-                right: 0;
-                bottom: 51px;
-                font-size: 16px;
-                color: #FFFFFF;
-                text-align: center;
-                margin-top: 300px;
-            }
-        }
+  width: 100%;
+  display: flex;
+  height: calc(100vh);
+
+  .left {
+    width: 640px;
+    background: linear-gradient(170deg, #33ADFF 0%, #45A8FD 40%, #9D2EFF 100%);
+
+    .bg {
+      height: calc(100vh);
+      width: 640px;
+      background-image: url("@/utils/image/loginBg.png");
+      background-size: 100% 100%;
+      position: relative;
+
+      img {
+        position: absolute;
+        left: 50%;
+        top: 240px;
+        width: 240px;
+        height: 300px;
+        margin-left: -120px;
+        cursor: pointer;
+      }
+
+      .bgtext {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 51px;
+        font-size: 16px;
+        color: #FFFFFF;
+        text-align: center;
+        margin-top: 300px;
+      }
     }
-    .right {
-        width: 800px;
-        padding: 160px 200px;
-    }
+  }
+
+  .right {
+    width: 800px;
+    padding: 160px 200px;
+  }
 }
 </style>
